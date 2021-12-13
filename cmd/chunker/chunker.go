3beda94b98bea67ddaefbb8e7f2e88194bc5ecd9
Carl Johnson <me@carlmjohnson.net>
2021-12-13 13:21:17 -0500

    Remove props from geojson files

--- cmd/chunker/chunker.go
+++ cmd/chunker/chunker.go
@@ -5,27 +5,20 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/carlmjohnson/csv"
 	"github.com/paulmach/orb/geojson"
 )
 
 func main() {
-	csv := flag.String("csv", "", "")
 	geojson := flag.String("geojson", "", "")
 	format := flag.String("format", "", "")
 	flag.Parse()
-	if err := run(*csv, *geojson, *format); err != nil {
+	if err := run(*geojson, *format); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func run(csvname, geojsonname, format string) error {
-	m, err := readCSV(csvname)
-	if err != nil {
-		return err
-	}
-
+func run(geojsonname, format string) error {
 	data, err := os.ReadFile(geojsonname)
 	if err != nil {
 		return err
@@ -37,15 +30,9 @@ func run(csvname, geojsonname, format string) error {
 	}
 	for _, feat := range fc.Features {
 		district := feat.Properties["District_1"].(string)
-		props, ok := m[district]
-		if !ok {
-			return fmt.Errorf("missing district %q", district)
-		}
 		newfc := geojson.NewFeatureCollection()
 		newfeat := geojson.NewFeature(feat.Geometry)
-		for k, v := range props {
-			newfeat.Properties[k] = v
-		}
+		newfeat.Properties["district"] = district
 		newfc.Append(newfeat)
 		data, err = newfc.MarshalJSON()
 		if err != nil {
@@ -58,23 +45,4 @@ func run(csvname, geojsonname, format string) error {
 	}
 
 	return nil
-}
-
-func readCSV(csvname string) (map[string]map[string]string, error) {
-	csvf, err := os.Open(csvname)
-	if err != nil {
-		return nil, err
-	}
-	defer csvf.Close()
-
-	m := map[string]map[string]string{}
-	csvr := csv.NewFieldReader(csvf)
-	for csvr.Scan() {
-		row := csvr.Fields()
-		m[row["district"]] = row
-	}
-	if err = csvr.Err(); err != nil {
-		return nil, err
-	}
-	return m, nil
 }
